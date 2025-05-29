cb0b3f9b811a01bc341264614376307427a454db
darkMasterv <79690406+lightdarkmaster@users.noreply.github.com>
2025-05-29 08:46:48 +0800

    Practice

---
+++ bsorting.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+public class bsorting {
+    public static void main(String[] args){
+        int numlist[] = {1,99,789,56,22,3,77,6,5};
+
+        bubbleSort(numlist);
+
+        String res = Arrays.toString(numlist);
+        System.out.print(res);
+
+
+    }
+    public static void bubbleSort(int arr[]){
+        int arrLength = arr.length;
+        boolean swapped;
+
+        for(int i=0; i < arrLength -1; i++){
+            swapped = false;
+            for(int j=0; j< arrLength -  i - 1; j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    swapped = true;
+                }
+            }if(!swapped){
+                break;
+            }
+        }
+    }
+}
--- selectionSort.java
+++ selectionSort.java
@@ -14,14 +14,12 @@ public class selectionSort {
         for (int i = 0; i < n - 1; i++) {
             int minIndex = i;
 
-            // Find the index of the smallest element in the unsorted part
             for (int j = i + 1; j < n; j++) {
                 if (arr[j] < arr[minIndex]) {
                     minIndex = j;
                 }
             }
 
-            // Swap the found minimum element with the first unsorted element
             if (minIndex != i) {
                 int temp = arr[i];
                 arr[i] = arr[minIndex];
