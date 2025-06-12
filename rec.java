1a330a0318e58d7b5f40ab70a67775ee79206052
darkMasterv <79690406+lightdarkmaster@users.noreply.github.com>
2025-06-13 00:30:55 +0800

    Update Documentation

--- rec.java
+++ rec.java
@@ -3,15 +3,18 @@ import java.util.Scanner;
 
 public class rec{
     public static void main(String[] args){
+        //variables declarations..
         int num1 = 0;
         int num2 = 1;
         int nextNumber;
         int length;
 
+        //Scanner input accepts length of the fib sequence..
         Scanner input = new Scanner(System.in);
             System.out.println("Enter Length: ");
             length = input.nextInt();
 
+            //looping until length achieved
             for(int i = 1; i <=length; i++){
                 System.out.print(num1 + " ");
                 nextNumber = num1 + num2;
