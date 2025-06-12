afad119dbd13e25fafca763f96768f5835425516
darkMasterv <79690406+lightdarkmaster@users.noreply.github.com>
2025-06-12 20:57:27 +0800

    Change rec to fib

--- rec.java
+++ rec.java
@@ -1,15 +1,22 @@
 import java.util.Scanner;
-public class rec {
+
+
+public class rec{
     public static void main(String[] args){
+        int num1 = 0;
+        int num2 = 1;
+        int nextNumber;
+        int length;
 
         Scanner input = new Scanner(System.in);
-        int length;
-        System.out.println("Enter A number: ");
-        length = input.nextInt();
+            System.out.println("Enter Length: ");
+            length = input.nextInt();
 
-        for(int i=1; i <=length; i++){
-            System.out.println(i);
-
+            for(int i = 1; i <=length; i++){
+                System.out.print(num1 + " ");
+                nextNumber = num1 + num2;
+                num1 = num2;
+                num2 = nextNumber;
         }
     }
 }
