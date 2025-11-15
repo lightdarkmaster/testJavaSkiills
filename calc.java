529ea199e52871a331300b2af779c1fa5d4e3376
darkMasterv <79690406+lightdarkmaster@users.noreply.github.com>
2025-11-16 06:39:33 +0800

    Add Calculator:

---
+++ calc.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+class calc{
+    public static void main(String[] args) {
+        System.out.println("This is a sample Calcualtor");
+
+        double num1, num2, result, sum, difference, product, quotient;
+
+
+        try{
+            try (Scanner input = new Scanner(System.in)) {
+                System.out.print("Enter Num1: ");
+                num1 = input.nextDouble();
+
+                System.out.print("Enter Num2: ");
+                num2 = input.nextDouble();
+            }
+            sum = num1 + num2;
+            difference = num1 - num2;
+            product = num1 * num2;
+            quotient = num1 / num2;
+
+            System.out.println("Sum: " + sum);
+            System.out.println("Difference: " + difference);
+            System.out.println("Product: " + product);
+            System.out.println("Quotient: " + quotient);
+
+            
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+}
