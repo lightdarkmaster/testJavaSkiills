8a5cde2534a1371a9be9fb2d452e4e3f9df292e6
darkMasterv <79690406+lightdarkmaster@users.noreply.github.com>
2025-11-22 08:27:02 +0800

    Reconfigure bug

--- calc.java
+++ calc.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 class calc{
     public static void main(String[] args) {
-        System.out.println("This is a sample Calcualtor");
+        System.out.println("This is a sample Calculator");
 
         double num1, num2, result, sum, difference, product, quotient;
 
@@ -14,24 +14,44 @@ class calc{
 
                 System.out.print("Enter Num2: ");
                 num2 = input.nextDouble();
+
+                sum = calculateSum(num1, num2);
+                difference = calculateDifference(num1, num2);
+                product = calculateProduct(num1, num2);
+                quotient = calculateQuotient(num1, num2);
+
+                result = calculateResult(sum, difference, product, quotient);
+
+                System.out.println("Sum: " + sum);
+                System.out.println("Difference: " + difference);
+                System.out.println("Product: " + product);
+                System.out.println("Quotient: " + quotient);
+
+                System.out.print("Result: " + result);
             }
-            sum = num1 + num2;
-            difference = num1 - num2;
-            product = num1 * num2;
-            quotient = num1 / num2;
 
-            result = sum + difference + product + quotient;// Not Necessary..
-
-            System.out.println("Sum: " + sum);
-            System.out.println("Difference: " + difference);
-            System.out.println("Product: " + product);
-            System.out.println("Quotient: " + quotient);
-
-            System.out.print("Result: " + result);
-
-            
         }catch(Exception e){
             System.out.println(e);
         }
+    }
+
+    private static double calculateSum(double num1, double num2){
+        return num1 + num2;
+    }
+
+    private static double calculateDifference(double num1, double num2){
+        return num1 - num2;
+    }
+
+    private static double calculateProduct(double num1, double num2){
+        return num1 * num2;
+    }
+
+    private static double calculateQuotient(double num1, double num2){
+        return num1 / num2;
+    }
+
+    private static double calculateResult(double sum, double difference, double product, double quotient){
+        return sum + difference + product + quotient;
     }
 }
